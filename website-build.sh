cbc121f2f568f8aeb6819a89819ac127a3d0ac59
Adam Powers <apowers@ato.ms>
2019-12-10 13:39:31 -0800

    update stix data

--- website-build.sh
+++ website-build.sh
@@ -4,6 +4,11 @@ CWD=`pwd`
 # TODO:
 #   - check python version: must be >3.6
 
+# copy stix data
+./stix-validate.sh
+cp generic-attack-stix/example-attack.json generic-attack-website/data/stix/
+cp generic-attack-stix/example-attack.json generic-attack-website/data/stix/example-attack_old.json
+
 # setup attack-website
 echo "Building attack website..."
 cd "$CWD/generic-attack-website"
